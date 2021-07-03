fa581300c85fb08a7ccee4f822bf93264cb6a7c6
ismaila10 <lodia.ismaila@gmail.com>
2021-07-03 15:07:12 +0200

    Setter init Part 1

---
+++ ConsoleApp/Produit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Produit
+{
+    public string Name { get; init; }
+    public string Description { get; init; }
+    public float Price { get; init; }
+    public DateTime DateCreation { get; init; }
+}
+
+
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var user = new Produit
+            {
+                Name = "pc",
+                Description = "MacBook Pro",
+                Price = 1600,
+                DateCreation = DateTime.Now
+            };
+
+            // Error =>  Impossible de modifier les propriétés de la classe après initialisation equivaut à readonly
+            //user.name = "clavier";
         }
     }
 }
